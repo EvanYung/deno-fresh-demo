46c075926b868d5a1571f0599bb44ec7c0eeee5b
EvanYung <yqq_1993@163.com>
2022-08-03 23:17:09 +0800

    feat: page

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,21 +2,21 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $0 from './routes/[name].tsx'
-import * as $1 from './routes/api/joke.ts'
-import * as $2 from './routes/index.tsx'
-import * as $$0 from './islands/Counter.tsx'
+import * as $0 from "./routes/api/joke.ts";
+import * as $1 from "./routes/greet/[name].tsx";
+import * as $2 from "./routes/index.tsx";
+import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
   routes: {
-    './routes/[name].tsx': $0,
-    './routes/api/joke.ts': $1,
-    './routes/index.tsx': $2,
+    "./routes/api/joke.ts": $0,
+    "./routes/greet/[name].tsx": $1,
+    "./routes/index.tsx": $2,
   },
   islands: {
-    './islands/Counter.tsx': $$0,
+    "./islands/Counter.tsx": $$0,
   },
   baseUrl: import.meta.url,
-}
+};
 
-export default manifest
+export default manifest;
